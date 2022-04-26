8d20e2cf32314bf3923f841106c4ef496295b7d2
harshit <Harshit.foodvybe@outlook.com>
2022-04-26 20:59:46 +0530

    More modifications

--- CodeEditor/client/src/components/Rooms.jsx
+++ CodeEditor/client/src/components/Rooms.jsx
@@ -1,22 +1,10 @@
 import * as React from 'react';
-import axios from 'axios';
 import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Code from '@mui/icons-material/Code';
 import Links from './Links.jsx'
 const theme = createTheme();
